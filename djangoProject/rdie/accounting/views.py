8418c9d369d66da01ce92b597850103ce64b3144
xiaohutuxian111 <2398391057@qq.com>
2023-04-22 09:37:18 +0800

    数据到前端显示

--- djangoProject/rdie/accounting/views.py
+++ djangoProject/rdie/accounting/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 
+from .models import *
 
 
 def index(request):
-    return render(request, 'accounting/index.html')
+    # return render(request, 'accounting/index.html')
+    all_accounts = Account.objects.all()
+    sub_categories = SubGategory.objects.all()
+    context = {
+        'accounts': all_accounts,
+        'sub_categories': sub_categories
+    }
+    print(context)
+    return render(request, 'accounting/index.html', context)
